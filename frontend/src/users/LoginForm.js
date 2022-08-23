fdadfec35e2062d33011bb90cde574a44a804c58
Deb Koch <92552721+DebraMK@users.noreply.github.com>
2022-08-22 22:03:30 -0500

    sign-up and log-in authentication added

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -9,16 +9,29 @@ function LoginForm() {
     const { setCurrentUser } = useContext(CurrentUser)
 
     const [credentials, setCredentials] = useState({
-        email: '',
-        password: ''
+        email: ' ',
+        password: ' '
     })
 
     const [errorMessage, setErrorMessage] = useState(null)
 
     async function handleSubmit(e) {
-        e.preventDefault()
+        // e.preventDefault()
+        const response = await fetch(`http://localhost:5000/authentication/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+        const data = await response.json()
        
-
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            history.push('/')
+        } else {
+            setErrorMessage(data.message)
+        }
     }
 
     return (
